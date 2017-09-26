00561d1c0721fb565f7c8843fc3f13cfef86c1b3
devnoname120 <devnoname120+gh@gmail.com>
2017-09-26 14:28:40 +0200

    kuio

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <taihen.h>
 #include <psp2/net/net.h>
 #include <psp2/io/fcntl.h>
+#include <kuio.h>
 
 
 char* rules[][2] = {{"psm-runtime.np.dl.playstation.net", "23.67.253.169"},
@@ -22,28 +23,35 @@ int find_dns_rule(const char *hostname) {
 }
 
 int hook_sceNetResolverStartNtoa(int rid, const char *hostname, SceNetInAddr *addr, int timeout, int retry, int flags) {
-	int fd = sceIoOpen("ux0:data/dns_log.txt", SCE_O_WRONLY | SCE_O_APPEND | SCE_O_CREAT, 0777);
-	sceIoWrite(fd, "Entering StartNtoa", sizeof("Entering StartNtoa"));
+	int fd;
+        kuIoOpen("ux0:data/dns_log.txt", SCE_O_WRONLY | SCE_O_APPEND | SCE_O_CREAT, &fd);
+	kuIoWrite(fd, "Entering StartNtoa", sizeof("Entering StartNtoa"));
 	int result = TAI_CONTINUE(int, ref_sceNetResolverStartNtoa, rid, hostname, addr, timeout, retry, flags);
 	if(addr != NULL && hostname != NULL) {
 		int rule = find_dns_rule(hostname);
 		if (rule >= 0) {
 			int res = sceNetInetPton(SCE_NET_AF_INET, rules[1][rule], &(addr->s_addr));
 			if (res == 0) {
-				sceIoWrite(fd, "Leaving StartNtoa overriden\n", sizeof("Leaving StartNtoa overriden\n"));
-				sceIoClose(fd);
+				kuIoWrite(fd, "Leaving StartNtoa overriden\n", sizeof("Leaving StartNtoa overriden\n"));
+				kuIoClose(fd);
 				return 0;
 			}
 		}
 	}
-	sceIoWrite(fd, "Leaving StartNtoa\n", sizeof("Leaving StartNtoa\n"));
-	sceIoClose(fd);
+	kuIoWrite(fd, "Leaving StartNtoa\n", sizeof("Leaving StartNtoa\n"));
+	kuIoClose(fd);
 	return result;
 }
 
 void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start() {
+	int fd;
+	kuIoOpen("ux0:data/dns_log.txt", SCE_O_WRONLY | SCE_O_APPEND | SCE_O_CREAT, &fd);
+	kuIoWrite(fd, "Started\n", sizeof("Started\n"));
 	g_hook = taiHookFunctionExport(&ref_sceNetResolverStartNtoa, "SceNet", 0x6BF8B2A2, 0x424AE26, hook_sceNetResolverStartNtoa);
+	if (g_hook < 0)
+		kuIoWrite(fd, "Bad\n", sizeof("Bad\n"));
+	kuIoClose(fd);
 	return 0;
 }
 
